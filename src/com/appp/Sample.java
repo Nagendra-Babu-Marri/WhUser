e9b97a05188367c6ac81d42032214f958f1a6be7
Admin <Admin@192.168.0.10>
2018-05-10 09:54:10 +0530

    This Is Modification

--- src/com/appp/Sample.java
+++ src/com/appp/Sample.java
@@ -1,9 +1,23 @@
 package com.appp;
 
-@SuppressWarnings("unused")
 public class Sample {
 	
-	private int sid;
-	private String sname;
-
+	private Long sno;
+	private String name;
+	public Long getSno() {
+		return sno;
+	}
+	public void setSno(Long sno) {
+		this.sno = sno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Sample [sno=" + sno + ", name=" + name + "]";
+	}
 }
